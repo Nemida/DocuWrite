313ab3b4daa4011f2952694f42ca9c5649689d73
Nemida <144930141+Nemida@users.noreply.github.com>
2026-02-07 18:55:09 +0530

    feat: color tools implemented

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -21,111 +21,84 @@ import TextStyle from "@tiptap/extension-text-style";
 import { Color } from "@tiptap/extension-color";
 import Highlight from "@tiptap/extension-highlight";
 
-import TextAlign from "@tiptap/extension-text-align";
-
-import Link from "@tiptap/extension-link";
-
-import { useLiveblocksExtension } from "@liveblocks/react-tiptap";
-import { useStorage } from "@liveblocks/react";
-
 import { useEditorStore } from "@/store/use-editor-store";
-import { FontSizeExtensions } from "@/extensions/font-size";
-import { LineHeightExtension } from "@/extensions/line-height";
-import { Ruler } from "./ruler";
-import { Threads } from "./threads";
-import { LEFT_MARGIN_DEFAULT, RIGHT_MARGIN_DEFAULT } from "@/constants/margins";
 
-interface EditorProps {
-    initialContent?: string | undefined;
-}
+export const Editor = () => {
+  const { setEditor } = useEditorStore();
 
-export const Editor = ({ initialContent }: EditorProps) => {
-    const leftMargin =
-        useStorage((root) => root.leftMargin) ?? LEFT_MARGIN_DEFAULT;
-    const rightMargin =
-        useStorage((root) => root.rightMargin) ?? RIGHT_MARGIN_DEFAULT;
+  const editor = useEditor({
+    onCreate({ editor }) {
+      setEditor(editor);
+    },
+    onDestroy() {
+      setEditor(null);
+    },
+    onUpdate({ editor }) {
+      setEditor(editor);
+    },
+    onSelectionUpdate({ editor }) {
+      setEditor(editor);
+    },
+    onTransaction({ editor }) {
+      setEditor(editor);
+    },
+    onFocus({ editor }) {
+      setEditor(editor);
+    },
+    onBlur({ editor }) {
+      setEditor(editor);
+    },
+    onContentError({ editor }) {
+      setEditor(editor);
+    },
+    editorProps: {
+      attributes: {
+        style: "padding-left: 56px; padding-right: 56px;",
+        class:
+          "focus:outline-none print:boder-0 border bg-white border-editor-border flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text",
+      },
+    },
+    extensions: [
+      StarterKit,
+      Table,
+      TableCell,
+      TableHeader,
+      TableRow,
+      TaskList,
+      Image,
+      ImageResize,
+      Underline,
+      FontFamily,
+      TextStyle,
+      Color,
+      Highlight.configure({
+        multicolor: true,
+      }),
+      TaskItem.configure({ nested: true }),
+    ],
+    content: `
+        <table>
+          <tbody>
+            <tr>
+              <th>Name</th>
+              <th colspan="3">Description</th>
+            </tr>
+            <tr>
+              <td>Cyndi Lauper</td>
+              <td>Singer</td>
+              <td>Songwriter</td>
+              <td>Actress</td>
+            </tr>
+          </tbody>
+        </table>
+      `,
+  });
 
-    const liveblocks = useLiveblocksExtension({
-        initialContent,
-        offlineSupport_experimental: true,
-    });
-    const { setEditor } = useEditorStore();
-
-    const editor = useEditor({
-        immediatelyRender: false,
-        onCreate({ editor }) {
-            setEditor(editor);
-        },
-        onDestroy() {
-            setEditor(null);
-        },
-        onUpdate({ editor }) {
-            setEditor(editor);
-        },
-        onSelectionUpdate({ editor }) {
-            setEditor(editor);
-        },
-        onTransaction({ editor }) {
-            setEditor(editor);
-        },
-        onFocus({ editor }) {
-            setEditor(editor);
-        },
-        onBlur({ editor }) {
-            setEditor(editor);
-        },
-        onContentError({ editor }) {
-            setEditor(editor);
-        },
-        editorProps: {
-            attributes: {
-                style: `padding-left: ${leftMargin}px; padding-right: ${rightMargin}px;`,
-                class: "focus:outline-none print:boder-0 border bg-white border-editor-border flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text",
-            },
-        },
-        extensions: [
-            liveblocks,
-            StarterKit.configure({
-                history: false,
-            }),
-            Table,
-            TableCell,
-            TableHeader,
-            TableRow,
-            TaskList,
-            Image,
-            ImageResize,
-            Underline,
-            FontFamily,
-            TextStyle,
-            Color,
-            LineHeightExtension.configure({
-                types: ["heading", "paragraph"],
-                defaultLineHeight: "1.5",
-            }),
-            FontSizeExtensions,
-            TextAlign.configure({
-                types: ["heading", "paragraph"],
-            }),
-            Link.configure({
-                openOnClick: false,
-                autolink: true,
-                defaultProtocol: "https",
-            }),
-            Highlight.configure({
-                multicolor: true,
-            }),
-            TaskItem.configure({ nested: true }),
-        ],
-    });
-
-    return (
-        <div className="size-full overflow-x-auto bg-editor-bg px-4 print:p-0 print:bg-white print:overflow-visible">
-            <Ruler />
-            <div className="min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0">
-                <EditorContent editor={editor} />
-                <Threads editor={editor} />
-            </div>
-        </div>
-    );
+  return (
+    <div className="size-full overflow-x-auto bg-editor-bg px-4 print:p-0 print:bg-white print:overflow-visible">
+      <div className="min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0">
+        <EditorContent editor={editor} />
+      </div>
+    </div>
+  );
 };
--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -2,17 +2,18 @@ import { Editor } from "./editor";
 import { Toolbar } from "./toolbar";
 
 interface DocumentIdPageProps {
-    params: Promise<{ documentId: string }>;
+  params: Promise<{ documentId: string }>;
 }
 
 const DocumentIdPage = async ({ params }: DocumentIdPageProps) => {
-    const { documentId } = await params;
-    return (
-        <div className="min-h-screen bg-[#FAFBFD]">
-            <Toolbar />
-            <Editor></Editor>
-        </div>
-    );
+  const { documentId } = await params;
+
+  return (
+    <div className="min-h-screen bg-editor-bg">
+      <Toolbar />
+      <Editor />
+    </div>
+  );
 };
 
 export default DocumentIdPage;
